bab107ed4e9e49978d5b543f2c9cf428cb0ecf21
EmreDkc57 <emredkc57@gmail.com>
2021-12-26 22:09:08 +0300

    Quiz Activity Added

--- app/src/main/java/com/DkcStudios/Engrik2020/MainActivity.java
+++ app/src/main/java/com/DkcStudios/Engrik2020/MainActivity.java
@@ -5,13 +5,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.DkcStudios.Engrik2020.adapter.RecyclerView_Adapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RecyclerView_Adapter.MyListener {
 
     static String[] letters = {"A letter","B Letter","C Letter","D Letter","E Letter"
     ,"F Letter", "G Letter"};
@@ -26,16 +27,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+      initVariables();
+
+
+    }
+
+
+
+
+
+
+
+    void initVariables() {
         recyclerView = findViewById(R.id.recyclerView);
 
 
         letterList = new ArrayList<>(Arrays.asList(letters));
 
 
-        RecyclerView_Adapter recyclerViewAdapter = new RecyclerView_Adapter(letterList,this);
+        RecyclerView_Adapter recyclerViewAdapter = new RecyclerView_Adapter(letterList,this,this);
         recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
         recyclerView.setAdapter(recyclerViewAdapter);
 
+
+
+    }
+
+
+    @Override
+    public void CallBack(int position) {
+
+        Toast.makeText(this,String.valueOf(position),Toast.LENGTH_LONG).show();
+
+
+        System.out.println(position);
 
     }
 }
--- app/src/main/java/com/DkcStudios/Engrik2020/adapter/RecyclerView_Adapter.java
+++ app/src/main/java/com/DkcStudios/Engrik2020/adapter/RecyclerView_Adapter.java
@@ -1,10 +1,13 @@
 package com.DkcStudios.Engrik2020.adapter;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,19 +17,20 @@ import com.DkcStudios.Engrik2020.R;
 
 import java.util.ArrayList;
 
-public class RecyclerView_Adapter extends RecyclerView.Adapter<RecyclerView_Adapter.RowHolder> {
+public class RecyclerView_Adapter extends RecyclerView.Adapter<RecyclerView_Adapter.RowHolder> implements View.OnClickListener{
 
     ArrayList<String> letters;
     AppCompatActivity appCompatActivity;
     LayoutInflater layoutInflater;
-
+    MyListener myListener;
     String[] colors = {"#45B39D","#52BE80"
     ,"#EC7063","#F7DC6F","#EB984E","#197BDC","#7F8C8D","#9B59B6"};
 
 
-    public RecyclerView_Adapter(ArrayList<String> letters, AppCompatActivity appCompatActivity) {
+    public RecyclerView_Adapter(ArrayList<String> letters, AppCompatActivity appCompatActivity,MyListener myListener) {
         this.letters = letters;
         this.appCompatActivity = appCompatActivity;
+        this.myListener = myListener;
     }
 
     @NonNull
@@ -37,6 +41,8 @@ public class RecyclerView_Adapter extends RecyclerView.Adapter<RecyclerView_Adap
 
 
         View rootView = layoutInflater.inflate(R.layout.row_recycler,parent,false);
+        rootView.setOnClickListener(this);
+
 
 
 
@@ -57,6 +63,20 @@ public class RecyclerView_Adapter extends RecyclerView.Adapter<RecyclerView_Adap
     }
 
 
+    @Override
+    public void onClick(View v) {
+
+        int pos = (int) v.getTag();
+
+        myListener.CallBack(pos);
+
+
+
+
+
+    }
+
+
     public class RowHolder extends RecyclerView.ViewHolder {
 
         TextView letterText;
@@ -65,12 +85,16 @@ public class RecyclerView_Adapter extends RecyclerView.Adapter<RecyclerView_Adap
         public RowHolder(@NonNull View itemView) {
             super(itemView);
 
+
+
             letterText = itemView.findViewById(R.id.textView);
 
         }
 
 
         private void bind(ArrayList<String> letters,String[] colors, int position) {
+
+            itemView.setTag(position);
 
             itemView.setBackgroundColor(Color.parseColor(colors[position%8]));
 
@@ -83,5 +107,11 @@ public class RecyclerView_Adapter extends RecyclerView.Adapter<RecyclerView_Adap
 
     }
 
+
+    public interface MyListener {
+
+         void CallBack(int position);
+
+    }
 
 }
